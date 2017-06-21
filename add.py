e31253c95088863aebbbf0c366b3c0df4f66928e
hj <173247429@qq.comgit>
2017-06-21 14:09:59 +0800

    新增

--- add.py
+++ add.py
@@ -1,5 +1,38 @@
+#猜数字
+import random
+s = int(random.uniform(1,100))
+print(s)
+m = int(input('输入整数:'))
+while m != s:
+	if m > s:
+		print('大了')
+		m = int(input('输入整数:'))
+	if m < s:
+		print('小了')
+		m = int(input('输入整数:'))
+	if m == s:
+		print('OK')
+		break;
+
+'''输出'''
 print('100+200+300=', 100+200+300);
 print("hello")
-name = input()
-print(name)
+print('hhhh')
+# name = input()
+# print(name)
 
+list = ['runoob',786,2.23,'huangjian']
+tinylist = [123, 'john']
+print (list);
+print(list[1])
+print(list[1:])
+print(tinylist *2)
+print( tinylist+list )
+
+flag = False
+name = 'luren'
+if name == 'python':         # 判断变量否为'python'
+    flag = True          # 条件成立时设置标志为真
+    print ('welcome boss')    # 并输出欢迎信息
+else:
+    print (name)              # 条件不成立时输出变量名称
---
+++ learnPython1.py
@@ -0,0 +1,41 @@
+# 定义函数
+def printme(str):
+    print(str)
+    return;
+
+
+# 调用函数
+printme("我要自己打印的")
+
+
+# 传可变对象
+def changeme(myList):
+    "修改传入的列表"
+    myList.append([1, 22, 23, 24]);
+    print("函数内取值：", myList);
+    return
+
+
+# 调用changeme函数
+myList = [5, 55, 555];
+changeme(myList);
+print("函数外取值：", myList)
+
+'''不定长参数
+ 你可能需要一个函数能处理比当初声明时更多的参数。这些参数叫做不定长参数，
+ 和上述2种参数不同，声明时不会命名。基本语法如下：'''
+
+
+# 可写函数声明
+def printinfo(arg1, *vartuple):
+    "打印任何传入的参数"
+    print("输出：")
+    print(arg1)
+    for var in vartuple:
+        print(var)
+        return;
+
+
+# 调用printinfo
+printinfo(10)
+printinfo(270, 60, 70);
